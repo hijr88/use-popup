b44bdb561aaa20c311517e8d087af349a06c3174
yohan <hijr88@naver.com>
2022-10-11 14:24:10 +0900

    Context가 존재하지 않는 경우 에러 발생시키기

--- libs/popup/usePopup.tsx
+++ libs/popup/usePopup.tsx
@@ -2,7 +2,11 @@ import Context from "../popup/context";
 import { useContext, useMemo } from "react";
 
 const usePopup = () => {
-  const popupContext = useContext(Context)!;
+  const popupContext = useContext(Context);
+  if (popupContext == null) {
+    throw new Error("usePopup must be used within a PopupProvider");
+  }
+
   return useMemo(() => {
     return popupContext;
   }, [popupContext]);
